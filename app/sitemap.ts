719731bda079b5e8235d20e482e5f2e8c9a4fc29
Karim <karim.davoodi@bluescape.com>
2024-01-16 20:29:40 -0500

    update sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     const list = [];
     const baseUrl = "https://www.quranwiki.org";
     const chapterUrl = `${baseUrl}/firstPage/chapterPage?`;
+    const hadithBookUrl = `${baseUrl}/firstPage/hadithFirstPage/bookPage?`;
     const hadithUrl = `${baseUrl}/firstPage/hadithFirstPage/bookPage/chapterPage?`;
 
     list.push({ url: `${baseUrl}/firstPage` });
@@ -14,9 +15,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
     }
 
     for (const book of hadith) {
+        list.push({
+            url: `${hadithBookUrl}folder=${book.folder}&amp;chapterNumber=${book.chapterNumber}&amp;bookName=${book.title}&amp;bookAuthor=${book.author}`,
+            // folder=muslim&chapterNumber=57&bookName=Sahih+Muslim&bookAuthor=Imam+Muslim+ibn+al-Hajjaj+al-Naysaburi
+        });
         for (let i = 1; i <= book.chapterNumber; i++) {
             list.push({
-                url: `${hadithUrl}folder=${book.folder}&amp;chapter=${i}`,
+                url: `${hadithUrl}folder=${book.folder}&amp;chapterId=${i}&amp;chapterNumber=${book.chapterNumber}&amp;bookName=${book.title}&amp;bookAuthor=${book.author}`,
+                // folder=muslim&chapterId=9&chapterNumber=57&bookName=Sahih+Muslim&bookAuthor=Imam+Muslim+ibn+al-Hajjaj+al-Naysaburi
             });
         }
     }
